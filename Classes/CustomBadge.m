ea819124da56e37170931bf42346238aedb3da27
Alexey Demin <dnv1ex@ya.ru>
2013-08-26 14:54:32 +0400

    Hide zero badge

--- Classes/CustomBadge.m
+++ Classes/CustomBadge.m
@@ -135,7 +135,7 @@
 	}
 	self.frame = CGRectMake(self.frame.origin.x, self.frame.origin.y, retValue.width, retValue.height);
     self.badgeText = badgeAttributedString;
-    self.hidden = !badgeText.length;
+    self.hidden = !badgeText.length || [badgeText.string isEqualToString:@"0"];
 	[self setNeedsDisplay];
 }
 
